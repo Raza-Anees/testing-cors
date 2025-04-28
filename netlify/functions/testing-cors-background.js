34c088586fd5378c25de5f05c406b6c1f2fd29bf
Raza-Anees <razaanees567@gmail.com>
2025-04-29 01:18:29 +0500

    testing

--- netlify/functions/testing-cors-background.js
+++ netlify/functions/testing-cors-background.js
@@ -1,6 +1,13 @@
 export const handler = async (event, context) => {
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*", // Allow all origins
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Allow methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow headers
+      }
     return {
       statusCode: 200,
+      headers
       body: JSON.stringify({ message: "Hello from Netlify Background Functions!" }),
     };
   };
