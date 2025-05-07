15a305617b7d1cfd0c8782c8a5fbd67aa9cba0bd
Raza-Anees <razaanees567@gmail.com>
2025-05-08 01:21:59 +0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,6 @@ function App() {
     const response=fetch('https://testingcorss.netlify.app/.netlify/functions/testing-cors-background', {
   method: 'POST',
   headers: { 
-    "Access-Control-Allow-Origin": "*", // Allow all origins for now
-    "Access-Control-Allow-Headers": "Content-Type, Origin, Accept",
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Content-Type": "application/json",
   },
 })
