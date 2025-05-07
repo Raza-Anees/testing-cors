ec861b1e303b72790a414f399496adb1f6982181
Raza-Anees <razaanees567@gmail.com>
2025-05-08 01:11:00 +0500

    testig

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,22 @@ import React, { useEffect } from 'react'
 function App() {
 
   useEffect(()=>{
-    const process=fetch('https://testingcorss.netlify.app/api/testing-cors-background', {
+//     const process=fetch('https://testingcorss.netlify.app/api/testing-cors-background', {
       
-  method: 'POST',
-  headers: { 
-    "Content-Type": "application/json",
+//   method: 'POST',
+//   headers: { 
+//     "Content-Type": "application/json",
 
-  },
+//   },
+// })
+// .then(response => response.json())
+// .catch(error => console.error('Error:', error));
+    const response=fetch('https://testingcorss.netlify.app/.netlify/functions/testing-cors-background', {
+  method: 'POST',
 })
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(error => console.error('Error:', error));
-//     fetch('https://testingcorss.netlify.app/.netlify/functions/testing-cors-background', {
-//   method: 'POST',
-// })
-// .then(response => response.json())
-// .then(data => console.log(data))
-// .catch(error => console.error('Error:', error));
   },[])
   return (
     <>
