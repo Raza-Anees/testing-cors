c48740018083cbb2f6ca036255ece5ae2ccaa22d
Raza-Anees <razaanees567@gmail.com>
2025-05-08 01:17:17 +0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ function App() {
 // .catch(error => console.error('Error:', error));
     const response=fetch('https://testingcorss.netlify.app/.netlify/functions/testing-cors-background', {
   method: 'POST',
+  headers: { 
+    "Access-Control-Allow-Origin": "*", // Allow all origins for now
+    "Access-Control-Allow-Headers": "Content-Type, Origin, Accept",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Content-Type": "application/json",
+  },
 })
 .then(response => response.json())
 .then(data => console.log(data))
